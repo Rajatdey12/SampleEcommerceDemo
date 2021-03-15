3ea162ce923f96a2a3e5d5ee816c6e05522dea71
Rajat Dey <giet13me131@gmail.com>
2021-03-15 11:03:35 +0530

    externalized the swgger details

--- sampleApp/sampleApp/src/main/java/com/rajat/eCommerce/sampleApp/config/SwaggerConfig.java
+++ sampleApp/sampleApp/src/main/java/com/rajat/eCommerce/sampleApp/config/SwaggerConfig.java
@@ -1,5 +1,6 @@
 package com.rajat.eCommerce.sampleApp.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -14,11 +15,24 @@ import static springfox.documentation.builders.PathSelectors.regex;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+
+    @Value("${swagger.package}")
+    private String controllerPackage;
+
+    @Value("${swagger.contact.name}")
+    private String name;
+
+    @Value("${swagger.contact.url}")
+    private String url;
+
+    @Value("${swagger.contact.email}")
+    private String email;
+
     @Bean
     public Docket productApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.rajat.eCommerce.sampleApp.controller"))
+                .apis(RequestHandlerSelectors.basePackage(controllerPackage))
                 .paths(regex("/.*"))
                 .build()
                 .apiInfo(metaData());
@@ -31,7 +45,7 @@ public class SwaggerConfig {
                 "Spring Boot REST API for E-commerce Store",
                 "1.0",
                 "Terms of service",
-                new Contact("Rajat Dey", "https://springframework.guru/about/", "rajat@springfrmework.guru"),
+                new Contact(name, url, email),
                 "Apache License Version 2.0",
                 "https://www.apache.org/licenses/LICENSE-2.0");
         return apiInfo;
